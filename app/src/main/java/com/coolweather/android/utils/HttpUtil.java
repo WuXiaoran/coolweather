b3367ac6d37a9805180ab7a771ebd854a2b003b4
xiaoran <1209255038@qq.com>
2017-02-22 16:00:46 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/coolweather/android/utils/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.utils;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 2017/2/22.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
